5132f477f21df25cde62e1a0bdd25e076d1720d9
pjh5020 <123628540+pjh5020@users.noreply.github.com>
2024-09-02 15:06:20 +0900

    add: setting

---
+++ src/main/java/com/capston/ServletInitializer.java
@@ -0,0 +1,18 @@
+package com.capston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CapstonApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstonApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/capston/StartController.java
@@ -0,0 +1,12 @@
+package com.capston;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+    @GetMapping("/")
+    public String ex(){
+        return "ex";
+    }
+}
